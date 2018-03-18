15c69b8e425e8b7209db68a922650327283a4dfa
League <League@AdminisorsiMac2.attlocal.net>
2018-03-18 16:01:05 -0700

    CircleRing, EvilPolygons

--- src/CircleRing.java
+++ src/CircleRing.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 0
@@ -6,29 +7,34 @@
 import org.jointheleague.graphical.robot.Robot;
 
 public class CircleRing {
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-        // 1. Make a new Robot
+		Robot Rob = new Robot();
 
+		Rob.setX(150);
+		
+		Rob.setY(200);
 
-       //  2. Set your robot’s position to x=150 and y=200
+		Rob.penDown();
 
-        // 3. Put the robot's pen down
+		Rob.setSpeed(10);
 
-        // 4. Set the robot’s speed to 10
+		for (int i = 0; i < 360; i++) {
 
-        // 5. Do everything below here 360 times (use i as the counter)
+			Rob.move(3);
 
-	        // 6. Move the robot 3 pixels
+			Rob.turn(1);
+			
+			if (i % 20 == 0) {
+				
+				for (int j = 0; j < 360; j++) {
+					
+					Rob.move(1);
+					
+					Rob.turn(1);
+				}
+			}
+		}
 
-       //  7. Turn the robot 1 degree
-
-        // 8. If the counter i is divisible by 20 (hint: use mod operator  %)..
-
-		// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
-
-				// 10. Move the robot 1 pixel
-
-				// 11. Turn the robot 1 degree
-}
+	}
 }
--- src/EvilPolygonsChallenge.java
+++ src/EvilPolygonsChallenge.java
@@ -5,23 +5,68 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class EvilPolygonsChallenge {
-	
+
 	public static void main(String[] args) {
-		//1. Create a robot
 
-		//2. Set the speed to 100
+		Robot Rob = new Robot();
 
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
-		
-		//3. Set the pen color of the robot according to the value of the colorChoise variable
-		
-		//4. Ask the use how many polygons they want to be drawn.
-		
-		//5. Use the robot to draw the number of polygons the user requested.
-		
-		//6. Make it so your shapes do not overlap
+		Rob.setSpeed(100);
 
-		//7. Challenge: add more colors to the Option Dialog.
+		int colorChoice = JOptionPane.showOptionDialog(null, "hello", null, JOptionPane.INFORMATION_MESSAGE,
+				JOptionPane.DEFAULT_OPTION, null, new String[] { "Red", "Green", "Blue" }, 0);
+
+		if (colorChoice == 0) {
+			Rob.penDown();
+			Rob.setPenColor(216, 39, 39);
+			String NP = JOptionPane.showInputDialog("How many polygons do you want to be drawn");
+			int NP1 = Integer.parseInt(NP);
+			for (int i = 0; i < NP1; i++) {
+				for (int j = 0; j < 4; j++) {
+					Rob.move(100);
+					Rob.turn(90);
+				}
+			}
+		}
+		if (colorChoice == 1) {
+			Rob.penDown();
+			Rob.setPenColor(28, 219, 50);
+			String NP = JOptionPane.showInputDialog("How many polygons do you want to be drawn");
+			int NP1 = Integer.parseInt(NP);
+			for (int i = 0; i < NP1; i++) {
+				for (int j = 0; j < 4; j++) {
+					Rob.move(100);
+					Rob.turn(90);
+				}
+			}
+		}
+		if (colorChoice == 2) {
+			Rob.penDown();
+			Rob.setPenColor(28, 145, 219);
+			String NP = JOptionPane.showInputDialog("How many polygons do you want to be drawn");
+			int NP1 = Integer.parseInt(NP);
+			for (int i = 0; i < NP1; i++) {
+				for (int j = 0; j < 3; j++) {
+					Rob.move(100);
+					Rob.turn(120);
+				}
+				
+				Rob.turn(100);
+			}
+		}
+		// 3. Set the pen color of the robot according to the value of the colorChoice
+		// variable
+
+		// 4. Ask the use how many polygons they want to be drawn.
+
+		// 5. Use the robot to draw the number of polygons the user requested.
+
+		// 6. Make it so your shapes do not overlap
+
+		// 7. Challenge: add more colors to the Option Dialog.
+
+	}
+
+	static void square() {
+
 	}
 }
-
