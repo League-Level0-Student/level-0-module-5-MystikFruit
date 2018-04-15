b6002d7a15b1019f0246edaf9ddae85c5542cbe1
catsrock <league@WTS14-4.attlocal.net>
2018-04-15 15:57:20 -0700

    GuessingPi, GoofyNames

--- src/GuessingPi.java
+++ src/GuessingPi.java
@@ -8,38 +8,107 @@ import javax.swing.JOptionPane;
 
 public class GuessingPi {
 	public static void main(String[] args) {
-		String pi = "314";
+		String pi = "3141592653589";
 
 		System.out.println(pi.charAt(0) + "" + pi.charAt(1) + "" + pi.charAt(2));
 		for (int i = 0; i < 999999999; i++) {
 			String pi1 = JOptionPane.showInputDialog("What's the next digit of pi?");
+			int pi2 = Integer.parseInt(pi1);
+			if (pi2 == 1) {
+				JOptionPane.showMessageDialog(null, "Correct!");
+				System.out.println(pi.charAt(3));
+				String pi3 = JOptionPane.showInputDialog("What's the next digit of pi?");
+				int pi4 = Integer.parseInt(pi3);
+				if (pi4 == 5) {
+					JOptionPane.showMessageDialog(null, "Correct!");
+					System.out.println(pi.charAt(4));
+					String pi5 = JOptionPane.showInputDialog("What's the next digit of pi?");
+					int pi6 = Integer.parseInt(pi5);
+					if (pi6 == 9) {
+						JOptionPane.showMessageDialog(null, "Correct!");
+						System.out.println(pi.charAt(5));
+						String pi7 = JOptionPane.showInputDialog("What's the next digit of pi?");
+						int pi8 = Integer.parseInt(pi7);
+						if (pi8 == 2) {
+							JOptionPane.showMessageDialog(null, "Correct!");
+							System.out.println(pi.charAt(6));
+							String pi9 = JOptionPane.showInputDialog("What's the next digit of pi?");
+							int pi10 = Integer.parseInt(pi9);
+							if (pi10 == 6) {
+								JOptionPane.showMessageDialog(null, "Correct!");
+								System.out.println(pi.charAt(7));
+								String pi11 = JOptionPane.showInputDialog("What's the next digit of pi?");
+								int pi12 = Integer.parseInt(pi11);
+								if (pi12 == 5) {
+									JOptionPane.showMessageDialog(null, "Correct!");
+									System.out.println(pi.charAt(8));
+									String pi13 = JOptionPane.showInputDialog("What's the next digit of pi?");
+									int pi14 = Integer.parseInt(pi13);
+									if (pi14 == 3) {
+										JOptionPane.showMessageDialog(null, "Correct");
+										System.out.println(pi.charAt(9));
+										String pi15 = JOptionPane.showInputDialog("What's the next digit of pi?");
+										int pi16 = Integer.parseInt(pi15);
+										if (pi16 == 5) {
+											JOptionPane.showMessageDialog(null, "Correct!");
+											System.out.println(pi.charAt(10));
+											String pi17 = JOptionPane.showInputDialog("What's the next digit of pi?");
+											int pi18 = Integer.parseInt(pi17);
+											if (pi18 == 8) {
+												JOptionPane.showMessageDialog(null, "Correct!");
+												System.out.println(pi.charAt(11));
+												String pi19 = JOptionPane
+														.showInputDialog("What's the next digit of pi?");
+												int pi20 = Integer.parseInt(pi19);
+												if (pi20 == 9) {
+													JOptionPane.showMessageDialog(null, "Correct!");
+													System.out.println(pi.charAt(12));
+													JOptionPane.showMessageDialog(null, "You have completed the game.");
+													JOptionPane.showMessageDialog(null,
+															"You now know the first 13 digits of pi.");
+													break;
+												} else {
+													JOptionPane.showMessageDialog(null, "Wrong!");
+													break;
+												}
+											} else {
+												JOptionPane.showMessageDialog(null, "Wrong!");
+												break;
+											}
+										} else {
+											JOptionPane.showMessageDialog(null, "Wrong!");
+											break;
+										}
+									} else {
+										JOptionPane.showMessageDialog(null, "Wrong!");
+										break;
+									}
+								} else {
+									JOptionPane.showMessageDialog(null, "Wrong!");
+									break;
+								}
+							} else {
+								JOptionPane.showMessageDialog(null, "Wrong!");
+								break;
+							}
+
+						} else {
+							JOptionPane.showMessageDialog(null, "Wrong!");
+							break;
+						}
+
+					} else {
+						JOptionPane.showMessageDialog(null, "Wrong!");
+						break;
+					}
+				} else {
+					JOptionPane.showMessageDialog(null, "Wrong!");
+					break;
+				}
+			} else {
+				JOptionPane.showMessageDialog(null, "Wrong!");
+				break;
+			}
 		}
 	}
-	// 1. Make a main method and make sure your program can run
-
-	// 2. Make a String variable to hold the value of Pi.
-	// Get the first few digits from http://www.piday.org/million/.
-
-	// 3. Print out the first 3 digits of Pi to the console.
-	// The first value is "pi.charAt(0)", the second is "pi.charAt(1)", etc.
-	// Run your program to see how this works.
-
-	// 9. If you want to give the user more than one chance to guess,
-	// put a for loop around steps 4-8.
-
-	// 4. Create a for loop that will step through each digit of pi (steps 5-8)
-	// NOTE: The number of digits in your string will be
-	// yourStringVariable.length()
-
-	// 5. Ask the user for the NEXT digit of pi.
-
-	// 6. Compare the user's input to the next digit of your pi variable
-	// (look at step 3 for a clue).
-	// HINT: use charAt(0) to get 1st char of user input String
-
-	// 7. If they are correct, print out "correct".
-
-	// 8. If they are not, print out "incorrect" and tell them
-	// to start over. Use 'break;' to break out of the loop.
-
 }
