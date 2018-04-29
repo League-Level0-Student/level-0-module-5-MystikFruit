92d061f59bd6cd3196995b035fb5b5d68bae718e
Asher Tedford <league@WTS-iMac-11.attlocal.net>
2018-04-29 15:57:58 -0700

    PrimeOrNot

--- src/PrimeOrNot.java
+++ src/PrimeOrNot.java
@@ -1,28 +1,17 @@
 import javax.swing.JOptionPane;
 
 public class PrimeOrNot {
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
 		for (int i = 0; i < 999999999; i++) {
 			String num = JOptionPane.showInputDialog("Tell me a number, and I'll not you if it's prime or not.");
 			int num1 = Integer.parseInt(num);
-			if (num1 % 2 == 1) {
-				JOptionPane.showMessageDialog(null, num1 + " is a prime number.");
-			} else if (num1 % 3 == 0) {
-				JOptionPane.showMessageDialog(null, num1 + " is not a prime number. It's a composite number.");
-			} else if (num1 % 4 == 0) {
-				JOptionPane.showMessageDialog(null, num1 + " is not a prime number. It's a composite number.");
-			} else if (num1 % 5 == 0) {
-				JOptionPane.showMessageDialog(null, num1 + " is not a prime number. It's a composite number.");
-			} else if (num1 % 6 == 0) {
-				JOptionPane.showMessageDialog(null, num1 + " is not a prime number. It's a composite number.");
-			} else if (num1 % 7 == 0) {
-				JOptionPane.showMessageDialog(null, num1 + " is not a prime number. It's a composite number.");
-			} else if (num1 % 8 == 0) {
-				JOptionPane.showMessageDialog(null, num1 + " is not a prime number. It's a composite number.");
-			} else if (num1 % 9 == 0) {
-				JOptionPane.showMessageDialog(null, num1 + " is not a prime number. It's a composite number.");
-			} else {
-				JOptionPane.showMessageDialog(null, num1 + " is a prime number.");
+			for (int j = 1; j < num1/2; j++) {
+				if (num1 % j == 0) {
+					JOptionPane.showMessageDialog(null, num1 + " is a prime number.");
+				} else {
+					JOptionPane.showMessageDialog(null, num1 + " is not a prime number. It is a composite number.");
+				}
+
 			}
 		}
 	}
